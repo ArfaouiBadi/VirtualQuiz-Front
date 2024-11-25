d8c64bd6f8757b6bdfbefd9a2b9bced27811c4ba
arfaouibadi <arfaouibadi19@gmail.com>
2024-11-25 12:14:26 +0100

    final touches

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { DataService } from '../service/data.service';
 
 @Component({
@@ -8,14 +9,47 @@ import { DataService } from '../service/data.service';
 })
 export class QuizComponent {
   username: string = '';
+  loading: boolean = false;
+  startQuizDisabled: boolean = true;
+  selectedFile: File | null = null;
 
-  constructor(private dataService: DataService) {}
+  constructor(private http: HttpClient, private dataSerivce: DataService) {}
+
+  onFileSelected(event: any): void {
+    const file: File = event.target.files[0];
+    if (file) {
+      this.selectedFile = file;
+      this.checkStartQuizEnabled();
+    }
+  }
+
+  checkStartQuizEnabled(): void {
+    this.startQuizDisabled = !(this.username && this.selectedFile);
+  }
 
   startQuiz(): void {
-    this.dataService
-      .startQuiz(this.username)
-      .subscribe((response: { status: any }) => {
-        console.log(response.status);
-      });
+    if (this.selectedFile) {
+      this.uploadFile(this.selectedFile);
+    }
+  }
+
+  uploadFile(file: File): void {
+    const formData = new FormData();
+    formData.append('file', file);
+
+    this.loading = true;
+    this.http.post('http://localhost:5000/upload-pdf', formData).subscribe(
+      (response) => {
+        console.log('File uploaded successfully', response);
+        this.loading = false;
+        this.dataSerivce.startQuiz(this.username).subscribe((data) => {
+          console.log('Quiz started', data);
+        });
+      },
+      (error) => {
+        console.error('Error uploading file', error);
+        this.loading = false;
+      }
+    );
   }
 }
